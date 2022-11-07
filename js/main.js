0bbd34cbb6d35fa3963f7c98fd2a3d70652a4135
Ivasska <113020143+Ivasska@users.noreply.github.com>
2022-11-07 18:41:25 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$ (function(){
+
+	$('.top__slider').slick({
+		arrows: false,
+		dots: true,
+		autoplay: true,
+		fade: true,
+	});
+
+	$('.reviews__slider').slick({
+		arrows: false,
+		dots: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+	});
+
+});
